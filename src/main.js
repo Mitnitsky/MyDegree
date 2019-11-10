7d22e33337eb019b3d4774c5c1ab7652101d6c94
Vladimir-pa <vov4ikpa@gmail.com>
2019-11-10 20:51:20 +0200

    Removed old project files, start working on firestore

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import BootstrapVue from 'bootstrap-vue'
 import Autocomplete from '@trevoreyre/autocomplete-vue'
 import '@trevoreyre/autocomplete-vue/dist/style.css'
 import firebase from 'firebase'
+import VueFirestore from 'vue-firestore'
 
 import {firebaseConfig} from './firebaseconfig'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -12,16 +13,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 //vuex
 import {store} from "./store/store";
+//firestore
+require('firebase/firestore');
 
+Vue.use(VueFirestore);
 Vue.use(Autocomplete);
 Vue.use(VModal);
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
+
 new Vue({
     created() {
         firebase.initializeApp(firebaseConfig)
+        const firestore = firebase.firestore();
+
     },
     store,
     render: h => h(App),
